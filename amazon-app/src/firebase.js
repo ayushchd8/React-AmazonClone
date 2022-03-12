141b295c0982959dafe0a323f214b9d3ba6a427d
Ayush <ayushchd8@gmail.com>
2022-03-12 20:35:55 +0530

    react-app new added files

---
+++ amazon-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDE4ApT8zyNnWE88UNF_mV9YwMgmSr2RKQ",
+    authDomain: "app-5d2f0.firebaseapp.com",
+    projectId: "app-5d2f0",
+    storageBucket: "app-5d2f0.appspot.com",
+    messagingSenderId: "903267707453",
+    appId: "1:903267707453:web:818c4ca81c18cb1c095854",
+    measurementId: "G-NN22QFKP8M"
+  };
